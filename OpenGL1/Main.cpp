ba3fd3e7b8aafe1bf7bc8c174f3ff25850246c7e
Bluepr1nt <martijncaluwe@gmail.com>
2016-04-21 10:56:32 +0200

    window

--- OpenGL1/Main.cpp
+++ OpenGL1/Main.cpp
@@ -1,6 +1,20 @@
 #include <GL/freeglut.h>
 
+
+
+void display()
+{
+	glutSwapBuffers();
+}
+
 int main(int argc, char* argv[])
 {
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowPosition(800, 600);
+	glutInit(&argc, argv);
+
+	glutDisplayFunc(display);
+
+	glutCreateWindow("Hello World");
+	glutMainLoop();
 }
